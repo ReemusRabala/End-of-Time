33aeb07f59087a4060962cb180339b51151dd36a
Reemus Räbälä <nagu@null.net>
2017-11-06 20:13:43 +0200

    Added preliminary input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("End of Time Game");
+    println!("What is your name?");
+
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input)
+        .expect("Failed to real line");
+
+    println!("You guessed: {}", input);
 }
